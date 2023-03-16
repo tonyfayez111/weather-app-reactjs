5d846eeb4c060080de4a9f98f5b818c310737241
Tonyfayez <tonyfayez111@gmail.com>
2023-03-16 20:59:56 +0200

    after some changes

--- src/Cards/Card/Card.js
+++ src/Cards/Card/Card.js
@@ -3,11 +3,14 @@ import React from "react";
 import "./card.css";
 const Card = (props) => {
   let data = props.data;
+  console.log(data);
   let condition = data.current.condition.text;
   return (
     <div className="weathercard">
       <div className="location">
-        <div className="country">{data.location.country}</div>
+        <div className="country">
+          {data.location.country},{data.location.name}
+        </div>
         <div className="temp">{data.current.temp_c}°C</div>
         <div>
           <img src={data.current.condition.icon} />
@@ -27,7 +30,6 @@ const Card = (props) => {
           Windspeed <br /> {data.current.wind_kph} Kph
         </div>
         <div>
-          {" "}
           Feels like <br />
           {data.current.feelslike_c}°C
         </div>
